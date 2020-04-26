1d6848bec71158ffb08abc5405066ce495b2eb76
Szymon-Przybylski <sprzybylski43@gmail.com>
2020-04-26 21:44:06 +0200

    Dodaj pliki projektów.

---
+++ Asteroids.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace asteroids
+{
+    public class Asteroids : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        SpaceShip player;
+
+        int screenWidth;
+        int screenHeight;
+        Vector2 screenCenter;
+
+        public Asteroids()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            graphics.PreferredBackBufferWidth = 1360;
+            graphics.PreferredBackBufferHeight = 768;
+            graphics.ApplyChanges();
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            
+            Window.Title = "Asteroids";
+            screenCenter = new Vector2(graphics.PreferredBackBufferWidth / 2, graphics.PreferredBackBufferHeight / 2);
+            screenWidth = graphics.PreferredBackBufferWidth;
+            screenHeight = graphics.PreferredBackBufferHeight;
+
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            player = new SpaceShip(Content, screenCenter);
+        }
+
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+            Content.Unload();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+
+            player.Update(screenWidth, screenHeight);
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            player.Draw(spriteBatch);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace asteroids
+{
+    class SpaceShip
+    {
+        bool isDriving;
+        readonly Texture2D texture;
+        Vector2 position;
+        Vector2 direction;
+        float accel;
+        float angle;
+        public SpaceShip(ContentManager content, Vector2 screenCenter)
+        {
+            texture = content.Load<Texture2D>("aSpaceship");
+            position.X = screenCenter.X;
+            position.Y = screenCenter.Y;
+
+        }
+
+        public void Update(int screenWidth, int screenHeight)
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+                angle -= 0.1f;
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+                angle += 0.1f;
+            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                if (!isDriving)
+                {
+                    isDriving = true;
+                    accel = 1;
+                }
+                direction = new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle));
+                position += direction * accel;
+                accel += 0.2f;
+                if (accel >= 5)
+                    accel = 5;
+            }
+            else
+            {
+                isDriving = false;
+                accel -= 0.1f;
+            }
+            position += direction * accel;
+
+            if (accel <= 0)
+                accel = 0;
+
+            if (position.X > screenWidth)
+                position.X = 0;
+            if (position.X < 0)
+                position.X = screenWidth;
+            if (position.Y > screenHeight)
+                position.Y = 0;
+            if (position.Y < 0)
+                position.Y = screenHeight;
+
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        { 
+            spriteBatch.Draw(texture, new Rectangle((int)position.X, (int)position.Y, texture.Width, texture.Width), null, Color.White, angle, new Vector2(texture.Width / 2, texture.Height / 2), SpriteEffects.None, 0f);
+        }
+
+    }
+}
